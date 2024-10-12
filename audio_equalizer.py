77d8b451ef8eae196ebdcb8c82d2e50e147e413b
Pasi Pyrrö <pasi.pyrro@aalto.fi>
2024-10-12 19:32:06 +0300

    implement multiprocessed version

--- audio_equalizer.py
+++ audio_equalizer.py
@@ -1,44 +1,54 @@
 import time
 import soundcard as sc
-from threading import Thread
-from queue import Queue, Empty
+from queue import Empty
 
-output_speaker_name = "BenQ GW2765"
-
-speakers = sc.all_speakers()
-
-speaker = next(s for s in speakers if output_speaker_name in s.name)
+from multiprocessing import Process, Queue, Value
 
 sample_rate = 96000
-playback_seconds = 10
-input_speaker_name = "Digital Audio (S/PDIF)"
-block_size = 1024*4
-record_size = block_size * 10
+playback_seconds = 1000
+# input_speaker_name = "Digital Audio (S/PDIF)"
+input_speaker_name = "BenQ GW2765"
+block_size = 1024*16
+record_size = block_size * 4
 num_samples = playback_seconds * sample_rate
 num_iters = num_samples // record_size
+volume = 0.2
 
-mics = sc.all_microphones(include_loopback=True)
 
-loopback_mic = next(m for m in mics if input_speaker_name in m.name)
-
-audio_queue = Queue()
-playing = True
-
-def play_task():
-    with speaker.player(samplerate=sample_rate, blocksize=block_size) as sp:
-        while playing:
+def play_task(playing, audio_queue, sample_rate, block_size):
+    # output_speaker_name = "BenQ GW2765"
+    output_speaker_name = "Digital Audio (S/PDIF)"
+    speakers = sc.all_speakers()
+    speaker = next(s for s in speakers if output_speaker_name in s.name)
+    with speaker.player(samplerate=sample_rate.value, blocksize=block_size.value) as sp:
+        while playing.value:
             try:
                 data = audio_queue.get_nowait()
                 sp.play(data)
             except Empty:
-                time.sleep(0.001)
+                time.sleep(0.00001)
 
-play_thread = Thread(target=play_task)
-play_thread.start()
 
-with loopback_mic.recorder(samplerate=sample_rate, blocksize=block_size) as mic:
-    for _ in range(record_size):
-        audio_queue.put(mic.record(record_size))
+if __name__ == '__main__':
+    mics = sc.all_microphones(include_loopback=True)
 
-playing = False
-play_thread.join()
+    loopback_mic = next(m for m in mics if input_speaker_name in m.name)
+
+    audio_queue = Queue()
+    playing = Value('b', True)
+    sample_rate_shared = Value('i', sample_rate)
+    block_size_shared = Value('i', block_size)
+
+    play_process = Process(target=play_task,
+                           args=(playing, audio_queue,
+                                 sample_rate_shared,
+                                 block_size_shared))
+    
+    with loopback_mic.recorder(samplerate=sample_rate, blocksize=block_size) as mic:
+        try:
+            play_process.start()
+            for _ in range(num_iters):
+                audio_queue.put(mic.record(record_size) * volume)
+        finally:
+            playing.value = False
+            play_process.join()
